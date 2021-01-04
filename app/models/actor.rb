17a50629dfdfadddad1b4d2db7be8f6aabf1a245
Max Ribbans <ribbans.max@gmail.com>
2021-01-04 13:15:14 -0500

    Add average actor age method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :casts
   has_many :movies, through: :casts
+  default_scope { order(age: :asc)}
+
+  def self.average_age
+    self.average(:age)
+  end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe Actor, type: :model do
   describe "relationships" do
     it { should have_many :movies}
   end
+
+  describe "model methods" do
+    it "scopes by age" do
+      james = Actor.create(name: "James Earl Jones", age: 65)
+      carrie = Actor.create(name: "Carrie Fisher", age: 45)
+      mark = Actor.create(name: "Mark Hammill", age: 55)
+
+      expect(Actor.all.pluck(:name)).to eq([carrie.name, mark.name, james.name])
+    end
+
+    it "returns average age" do
+      james = Actor.create(name: "James Earl Jones", age: 65)
+      carrie = Actor.create(name: "Carrie Fisher", age: 45)
+      mark = Actor.create(name: "Mark Hammill", age: 55)
+
+      expect(Actor.average_age).to eq(55)
+      # janky test below- figure out why limit does not allow method to work
+      # skywalkers = Actor.limit(2)
+      # expect(skywalkers.average_age).to eq(50)
+    end
+  end
 end
